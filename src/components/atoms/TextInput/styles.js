bbb8cec9ef668a3be13afeaabed62f6238df2949
booldook <booldook@gmail.com>
2024-02-07 11:11:33 +0900

    css

--- src/components/atoms/TextInput/styles.js
+++ src/components/atoms/TextInput/styles.js
@@ -35,18 +35,16 @@ export const TemplateStyle = styled("div")`
   display: flex;
   position: relative;
   gap: 10px;
-  aligin-items: center;
+  align-items: center;
 `
 // 인풋 레이아웃 스타일
 export const TemplateInputStyle = styled("div")`
   display: flex;
   position: relative;
-  padding: 8px 12px;
   border: 1px solid #ccc;
-  border-radius: 4px;
   font-size: 16px;
   width: 100%;
   box-sizing: border-box;
-  padding-left: ${({ icons }) => (icons ? "40px" : "12px")};
-  border-radius: ${({ round }) => (round ? "50%" : "0")};
+  padding: 8px 12px 8px ${ ({ icons }) => (icons ? "40px" : "12px") };
+  border-radius: ${ ({ round }) => (round ? "50%" : "0") };
 `
