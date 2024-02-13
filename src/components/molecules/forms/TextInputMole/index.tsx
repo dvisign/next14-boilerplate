6879106ff9a28db93d2ae935fbac72a04c778161
dvisign <jmch0922@gmail.com>
2024-02-13 16:23:00 +0900

    컴포넌트 타입메모제이션 최적화

--- src/components/molecules/forms/TextInputMole/index.tsx
+++ src/components/molecules/forms/TextInputMole/index.tsx
@@ -1,54 +1,64 @@
-import { memo } from "react"
-import TextInput from "@/atoms/forms/TextInput"
+import React, { memo, ReactElement } from "react"
 import Label from "@/atoms/forms/Label"
+import TextInput from "@/atoms/forms/TextInput"
 import { TextWrapperMoleStyle, RoundTextInputMoleStyle, TextInputMoleStyle, IconStyle } from "./style"
 import { textInputMoleTypes } from "./types"
 
-// 라운드 필드
-const RoundInput = ({ type = "text", label, icons, className, id, onChange, value }: textInputMoleTypes) => {
-  return (
-    <TextWrapperMoleStyle className={className} id={id}>
-      <Label label={label} />
-      <RoundTextInputMoleStyle icons={icons ? true : false}>
-        {icons && <IconStyle>{icons}</IconStyle>}
-        <TextInput type={type} onChange={onChange} value={value} />
-      </RoundTextInputMoleStyle>
-    </TextWrapperMoleStyle>
-  )
-}
+// 라운드 필드 컴포넌트
+const RoundInput = memo(
+  ({
+    type = "text",
+    label = "",
+    icons = null,
+    className = "",
+    id = "",
+    onChange = () => null,
+    value = "",
+  }: textInputMoleTypes) => {
+    return (
+      <TextWrapperMoleStyle className={className} id={id}>
+        <Label label={label} />
+        <RoundTextInputMoleStyle icons={icons ? true : false}>
+          {icons && <IconStyle>{icons}</IconStyle>}
+          <TextInput type={type} onChange={onChange} value={value} />
+        </RoundTextInputMoleStyle>
+      </TextWrapperMoleStyle>
+    )
+  },
+  (prevProps, nextProps) => prevProps.value === nextProps.value,
+)
 
-// 입력 필드
-const TextInputMole = ({
-  type = "text",
-  label = "",
-  icons = null,
-  className = "",
-  id = "",
-  onChange = () => null,
-  value = "",
-}: textInputMoleTypes) => {
-  return (
-    <TextWrapperMoleStyle className={className} id={id}>
-      <Label label={label} />
-      <TextInputMoleStyle icons={icons ? true : false}>
-        {icons && <IconStyle>{icons}</IconStyle>}
-        <TextInput type={type} onChange={onChange} value={value} />
-      </TextInputMoleStyle>
-    </TextWrapperMoleStyle>
-  )
-}
+// 입력 필드 컴포넌트
+const TextInputMole = memo(
+  ({
+    type = "text",
+    label = "",
+    icons = null,
+    className = "",
+    id = "",
+    onChange = () => null,
+    value = "",
+  }: textInputMoleTypes) => {
+    return (
+      <TextWrapperMoleStyle className={className} id={id}>
+        <Label label={label} />
+        <TextInputMoleStyle icons={!!icons}>
+          {icons && <IconStyle>{icons}</IconStyle>}
+          <TextInput type={type} onChange={onChange} value={value} />
+        </TextInputMoleStyle>
+      </TextWrapperMoleStyle>
+    )
+  },
+  (prevProps, nextProps) => prevProps.value === nextProps.value,
+)
 
-TextInputMole.RoundInput = RoundInput
-
-const MemoTextInputMole = memo(TextInputMole, (prevProps, nextProps) => {
-  return prevProps.value === nextProps.value
-})
-
-interface CustomMemoTextInputMoleType extends React.MemoExoticComponent<(props: textInputMoleTypes) => JSX.Element> {
+// 커스텀 타입 정의
+interface CustomTextInputMoleType extends React.MemoExoticComponent<(props: textInputMoleTypes) => ReactElement> {
   RoundInput: typeof RoundInput
 }
 
-const CustomMemoTextInputMole = MemoTextInputMole as unknown as CustomMemoTextInputMoleType
+// 타입 캐스팅
+const CustomMemoTextInputMole: CustomTextInputMoleType = TextInputMole as any
 CustomMemoTextInputMole.RoundInput = RoundInput
 
 export default CustomMemoTextInputMole
