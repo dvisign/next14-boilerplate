788566131ea1cb83b277414e579ccabf893c5e93
jungmoonchai <moonchai.jung@speser.net>
2024-02-14 17:15:25 +0900

    forwordref 제거

--- src/components/atoms/forms/TextInput/index.tsx
+++ src/components/atoms/forms/TextInput/index.tsx
@@ -4,19 +4,29 @@ import { unstable_useForkRef as useForkRef } from "@mui/utils"
 import { InputBaseStyle } from "./styles"
 import { TextInputTypes } from "./types"
 
-const TextInput = forwardRef(function TextInput(props: TextInputTypes, ref: ForwardedRef<HTMLInputElement>) {
-  const { getRootProps, getInputProps } = useInput(props)
-  console.log("getRootProps", getRootProps())
-  console.log("getInputProps", getInputProps())
-  const inputProps = getInputProps()
+// const TextInput = forwardRef(function TextInput(props: TextInputTypes, ref: ForwardedRef<HTMLInputElement>) {
+//   const { getRootProps, getInputProps } = useInput(props)
+//   console.log("getRootProps", getRootProps())
+//   console.log("getInputProps", getInputProps())
+//   const inputProps = getInputProps()
 
-  inputProps.ref = useForkRef(inputProps.ref, ref)
+//   inputProps.ref = useForkRef(inputProps.ref, ref)
+
+//   return (
+//     <div {...getRootProps()}>
+//       <InputBaseStyle icons={props.icons} {...props} {...inputProps} />
+//     </div>
+//   )
+// })
+function TextInput(props: TextInputTypes) {
+  const { getRootProps, getInputProps } = useInput(props)
+  const inputProps = getInputProps()
 
   return (
     <div {...getRootProps()}>
       <InputBaseStyle icons={props.icons} {...props} {...inputProps} />
     </div>
   )
-})
+}
 
 export default TextInput
