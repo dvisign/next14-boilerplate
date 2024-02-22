6965c57c724b4822f386cb7720e9b13a4e8539f8
booldook <booldook@gmail.com>
2024-02-22 17:18:54 +0900

    hooksTypes 변경

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 import { useCallback, useState, ChangeEvent } from "react"
-import { UseInputReturnTypes } from "./hooks.types"
+import { UseInputReturnTypes } from "./hooksTypes"
 import color from "@/assets/scss/_color.module.scss"
 
 export const useInput = <T>(initialData: T): UseInputReturnTypes<T> => {
