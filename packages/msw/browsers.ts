a8e0975d14d8f67db4baae99b4cc27ad362abcf1
dvisign <jmch0922@gmail.com>
2024-02-26 17:59:01 +0900

    msw 리퀘스트 타입 작업중

--- packages/msw/browsers.ts
+++ packages/msw/browsers.ts
@@ -1,4 +1,9 @@
 import { setupWorker } from "msw/browser"
 import { handlers } from "./handlers"
+import { RequestHandler } from "msw"
 
+type HandlerType = RequestHandler<any, any>
 export const worker = setupWorker(...handlers)
+export function createWorker(handler: HandlerType[]) {
+  return setupWorker(...handler)
+}
