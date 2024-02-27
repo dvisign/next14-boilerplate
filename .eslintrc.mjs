a609d0f38ebf9521cd130dea81375bae4d3ad0bc
dvisign <jmch0922@gmail.com>
2024-02-27 17:06:31 +0900

    eslint 모듈수정

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -1,8 +1,8 @@
 // This configuration only applies to the package manager root.
 /** @type {import("eslint").Linter.Config} */
-module.exports = {
+export default {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  extends: ["@repo/eslint-config/library.mjs"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
