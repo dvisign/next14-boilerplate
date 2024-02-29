33e3c7d0b7ac423485eca07937a1c79b9abe2be5
dvisign <jmch0922@gmail.com>
2024-02-29 16:43:17 +0900

    지긋지긋한 eslint 2

--- apps/storybook/components/.eslintrc.js
+++ apps/storybook/components/.eslintrc.js
@@ -3,7 +3,7 @@ const { resolve } = require("path");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/__eslint/next.js"],
+  extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
