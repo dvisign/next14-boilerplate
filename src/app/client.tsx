249bcb8e0f9b1589d8ae6c7848d36ed76bedd2e2
dvisign <jmch0922@gmail.com>
2024-03-12 18:04:31 +0900

    패키지 빌드테스트 및 패지키 peerdependence확인

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -2,15 +2,26 @@
 import { ReactNode, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import { useAppSelector } from "@/redux/store"
+import { Buttons, ButtonTypes } from "@dvisign/ibj-packages"
 
 function Client({ children }: { children: ReactNode }) {
   const user = useAppSelector(state => state.user)
   const router = useRouter()
+  const test: ButtonTypes = {
+    children: "??",
+  }
+  console.log(test)
   useEffect(() => {
     if (!user.id && !user.name) router.push("/")
   }, [user])
   // 로그아웃일때 레이아웃
-  if (!user.name || !user.id) return <div>{children}</div>
+  if (!user.name || !user.id)
+    return (
+      <>
+        <Buttons>??</Buttons>
+        <div>{children}</div>
+      </>
+    )
   // 로그인 후 레이아웃
   return <>{children}</>
 }
